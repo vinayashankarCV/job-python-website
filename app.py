bc62c06b5d7caaf0e16419c94df074b4e6371715
vinayashankarCV <126003450+vinayashankarCV@users.noreply.github.com>
2023-05-28 11:33:21 +0000

    mode some change in app and HTML

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 
 
+
+JOBS=[
+  {
+    'id':1,
+    'title':'Data Analyst',
+    'location':'Bengluru,India',
+    'Salary':'Rs.10,00,000'
+  },
+  {
+    'id':2,
+    'title':'Data Scientist',
+    'location':'Delhi,India',
+    'Salary':'Rs.30,00,000'
+  },
+  {
+    'id':3,
+    'title':'Data Scientist',
+    'location':'Bengluru,India',
+    'Salary':'Rs.15,00,000'
+  }
+]
+
 @app.route("/")
 def hello_world():
-  return"Hello,world"
+  return render_template("home.html",jobs=JOBS,)
+
 
 
 if __name__=="__main__":
